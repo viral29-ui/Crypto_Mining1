76d340b16b61466464b6e16be1c016a6f62eaf63
rabic <rabidesk@gmail.com>
2021-01-03 00:52:44 +0800

    Phone Number tracking comment

--- Phone Number Tracking Location.py
+++ Phone Number Tracking Location.py
@@ -2,12 +2,14 @@
 import phonenumbers
 from test import number
 
-from phonenumbers import geocoder # geocoder function of phonenumbers
+from phonenumbers import geocoder # geocoder function of phoneying
 
-ch_nmber = phonenumbers.parse(number,"CH")
-print(geocoder.description_for_number(ch_nmber, "en"))
+ch_nmber = phonenumbers.parse(number,"CH") # variable = package phone number with parse for parameter C is for country and H for history
+print(geocoder.description_for_number(ch_nmber, "en")) # geocoder connect with number description with parameter with variable and language
 # To know service provider
-from phonenumbers import carrier
-service_nmber = phonenumbers.parse(number, "RO")
-print(carrier.name_for_number(service_nmber,"en"))
+from phonenumbers import carrier # carrier use for service provider name
+service_nmber = phonenumbers.parse(number, "RO") # RO stand for roaming
+print(carrier.name_for_number(service_nmber,"en")) # call carrier function and language name
+
+
 
