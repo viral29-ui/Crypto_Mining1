8b6ef551880592fa6d97fe10f647b58c1c9e577e
rabic <rabidesk@gmail.com>
2021-01-16 11:28:07 +0800

    Web Scraping

---
+++ Bit_Coin_Mining.py
@@ -0,0 +1,31 @@
+from hashlib import sha256
+
+
+def SHA256(text):
+    return (sha256("ABC".encode("ascii")).hexdigest())
+
+def mine():
+    pass
+
+if __name__=='__main__':
+    transactions = ''' 
+    print(SHA256("ABC"))
+    Mando->Cara-45
+    '''
+    new_hash = (transactions)
+
+    print(new_hash)
+
+# This is new
+
+
+
+
+
+
+
+
+
+
+
+
